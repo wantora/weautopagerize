49a501937761e3db8782d9e7d83ef0166b6fbee8
wantora <wantora@gmail.com>
2017-12-03 00:01:43 +0900

    Fix ScrollListener

--- src/lib/content/ScrollListener.js
+++ src/lib/content/ScrollListener.js
@@ -12,13 +12,17 @@ export default class ScrollListener {
         this._callback.call(null);
       }, 200);
     };
+    this._intervalId = null;
   }
   enable() {
     window.addEventListener("scroll", this._listener, {passive: true});
     window.addEventListener("resize", this._listener, {passive: true});
+    clearInterval(this._intervalId);
+    this._intervalId = setInterval(this._listener, 1000);
   }
   disable() {
     window.removeEventListener("scroll", this._listener, {passive: true});
     window.removeEventListener("resize", this._listener, {passive: true});
+    clearInterval(this._intervalId);
   }
 }
