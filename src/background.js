7bfb451f90f600853d8cd0ed66a9aba648fe02c4
wantora <wantora@gmail.com>
2018-04-03 00:46:03 +0900

    Remove SyncPrefs

--- src/background.js
+++ src/background.js
@@ -1,26 +1,27 @@
 import SiteinfoManager from "./lib/background/SiteinfoManager";
 import ExcludeList from "./lib/background/ExcludeList";
-import SyncPrefs from "./lib/background/SyncPrefs";
+import Prefs from "./lib/background/Prefs";
 import ButtonManager from "./lib/background/ButtonManager";
 
 (async () => {
   try {
     const siteinfoManager = new SiteinfoManager();
     const excludeList = new ExcludeList();
-    const syncPrefs = new SyncPrefs(["openLinkInNewTab"]);
     const buttonManager = new ButtonManager();
 
-    await Promise.all([siteinfoManager.init(), excludeList.init(), syncPrefs.init()]);
+    await Promise.all([siteinfoManager.init(), excludeList.init()]);
 
     browser.runtime.onMessage.addListener(async (message, sender) => {
       if (message) {
         if (message.type === "getData") {
+          const {openLinkInNewTab} = await Prefs.get(["openLinkInNewTab"]);
+
           const urlStr = message.value;
           const data = {
             userActive: !excludeList.check(urlStr),
             siteinfo: siteinfoManager.getSiteinfo(urlStr),
             prefs: {
-              openLinkInNewTab: syncPrefs.get("openLinkInNewTab"),
+              openLinkInNewTab,
             },
           };
           return data;
