c06f80ec6fde9f36ced89d235db36510eff87acb
wantora <wantora@gmail.com>
2025-09-29 08:45:32 +0900

    Update build.mjs

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,20 +1,38 @@
-import {rm, cp, watch} from "node:fs/promises";
+import {rm, cp, watch, readFile, writeFile} from "node:fs/promises";
+import {globSync} from "node:fs";
 import {join, basename} from "node:path";
-import {glob} from "glob";
 import esbuild from "esbuild";
 
 const CONFIG = {
-  entryPoints: glob.sync("./src/*.js"),
+  entryPoints: globSync("./src/*.js"),
   outdir: "./dist",
   copydir: "./src/webext",
   copyfiles: ["./README.md", "./LICENSE"],
 };
+
+async function copy() {
+  await cp(CONFIG.copydir, CONFIG.outdir, {recursive: true});
+
+  for (const file of CONFIG.copyfiles) {
+    await cp(file, join(CONFIG.outdir, basename(file)), {recursive: true});
+  }
+
+  const manifestFile = join(CONFIG.outdir, "manifest.json");
+  const manifest = await readFile(manifestFile, {
+    encoding: "utf8",
+  });
+  const packageJson = JSON.parse(
+    await readFile("package.json", {
+      encoding: "utf8",
+    })
+  );
+  await writeFile(
+    manifestFile,
+    manifest.replaceAll("__PACKAGEJSON_VERSION__", packageJson.version)
+  );
+}
+
 const watchMode = process.argv.includes("--watch");
-
-console.log("CONFIG =", CONFIG);
-
-await rm(CONFIG.outdir, {recursive: true, force: true});
-
 const ctx = await esbuild.context({
   entryPoints: CONFIG.entryPoints,
   bundle: true,
@@ -24,25 +42,19 @@ const ctx = await esbuild.context({
   mainFields: ["module", "browser", "main"],
 });
 
+console.log("CONFIG =", CONFIG);
+await rm(CONFIG.outdir, {recursive: true, force: true});
+await copy();
+
 if (watchMode) {
   await ctx.watch();
-} else {
-  await ctx.rebuild();
-  await ctx.dispose();
-}
 
-const cpDir = async () => {
-  for (const file of CONFIG.copyfiles) {
-    await cp(file, join(CONFIG.outdir, basename(file)), {recursive: true});
-  }
-  return cp(CONFIG.copydir, CONFIG.outdir, {recursive: true});
-};
-await cpDir();
-
-if (watchMode) {
   const watcher = watch(CONFIG.copydir, {recursive: true});
   for await (const event of watcher) {
     console.log(event);
-    await cpDir();
+    await copy();
   }
+} else {
+  await ctx.rebuild();
+  await ctx.dispose();
 }
